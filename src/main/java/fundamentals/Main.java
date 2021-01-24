ea751543f6e9dc1bd9d767e2be18107da170bed9
Balan-adrian <adri.balan21@gmail.com>
2021-01-24 10:42:15 +0200

    Task 1: BMI

--- src/main/java/fundamentals/Main.java
+++ src/main/java/fundamentals/Main.java
@@ -6,20 +6,20 @@ public class Main {
     public static void main(String[] args) {
 
         //static vs non-static fields and methods
-        TasksExercices tasksExercices1 = new TasksExercices();
-        TasksExercices tasksExercices2 = new TasksExercices();
-        System.out.println("x = " + tasksExercices1.x);
-        System.out.println("y = " + tasksExercices1.y);
-        System.out.println("y = " + tasksExercices2.y);
-        tasksExercices1.x = 22;
-        TasksExercices.y = 3;
-        System.out.println("x1 = " + tasksExercices1.x);
-        System.out.println("x2 = " + tasksExercices2.x);
-        System.out.println("y1 = " + tasksExercices1.y);
-        System.out.println("y2 = " + tasksExercices2.y);
-
-        //Task1
-        tasksExercices1.perimeters();
-
+//        TasksExercices tasksExercices1 = new TasksExercices();
+//        TasksExercices tasksExercices2 = new TasksExercices();
+//        System.out.println("x = " + tasksExercices1.x);
+//        System.out.println("y = " + tasksExercices1.y);
+//        System.out.println("y = " + tasksExercices2.y);
+//        tasksExercices1.x = 22;
+//        TasksExercices.y = 3;
+//        System.out.println("x1 = " + tasksExercices1.x);
+//        System.out.println("x2 = " + tasksExercices2.x);
+//        System.out.println("y1 = " + tasksExercices1.y);
+//        System.out.println("y2 = " + tasksExercices2.y);
+//
+//        //Task1
+//        tasksExercices1.perimeters();
+TasksExercices.BMI();
     }
 }
--- src/main/java/fundamentals/TasksExercices.java
+++ src/main/java/fundamentals/TasksExercices.java
@@ -3,19 +3,38 @@ package fundamentals;
 import java.util.Scanner;
 
 public class TasksExercices {
-    public static final float pi = (float) Math.PI;
+//    public static final float pi = (float) Math.PI;
+//
+//
+//    int x = 10;
+//    static int y = 1;
+//    //Write an application that will read diameter of a circle (variable of type float) and
+//    //calculate perimeter of given circle.
+//    //Firstly, assume π = 3.14. Later, use value of π from built-in Math class
+//
+//    public static void perimeters() {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Diametru este: ");
+//        float diametru = scanner.nextFloat();
+//        System.out.println("Perimetrul este " + diametru * pi);
+//    }
 
-
-    int x = 10;
-    static int y = 1;
-    //Write an application that will read diameter of a circle (variable of type float) and
-    //calculate perimeter of given circle.
-    //Firstly, assume π = 3.14. Later, use value of π from built-in Math class
-
-    public static void perimeters() {
+    //Write an application calculating BMI (Body Mass Index) and checking if itЀs optimal or not.
+    //Your application should read two variables: weight (in kilograms, type float) and height
+    //(in centimeters, type int). BMI should be calculated given following formula:
+    //The optimal BMI range is from 18.5 to 24.9, smaller or larger values are non-optimal
+    //values. Your program should write "BMI optimal" or "BMI not optimal", according to the
+    //assumptions above.
+    public static void BMI() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Diametru este: ");
-        float diametru = scanner.nextFloat();
-        System.out.println("Perimetrul este " + diametru * pi);
-    }
+        System.out.println("Insert your height in cm:");
+        int height = scanner.nextInt();
+        System.out.println("Insert your weight in kg:");
+        float weight = scanner.nextFloat();
+        float bmi = weight / ((float) height / 100 * (float) height / 100);
+        if ((bmi > 18.5f) && (bmi < 24.9)){
+            System.out.println(bmi + "BMI optional");}
+        else {
+            System.out.println(bmi + "BMI not optional");
+    }}
 }
