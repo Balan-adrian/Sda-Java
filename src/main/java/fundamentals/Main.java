9c15233192f47d42d1eee3724561b74d0a343547
Balan-adrian <adri.balan21@gmail.com>
2021-01-24 11:07:38 +0200

    Task 1: FizzBuzz

--- src/main/java/fundamentals/Main.java
+++ src/main/java/fundamentals/Main.java
@@ -22,5 +22,6 @@ public class Main {
 //        tasksExercices1.perimeters();
 //TasksExercices.BMI();
 TasksExercices.fizzBuzz();
+
     }
 }
--- src/main/java/fundamentals/TasksExercices.java
+++ src/main/java/fundamentals/TasksExercices.java
@@ -61,6 +61,5 @@ public class TasksExercices {
              System.out.println(i);
          }
         }
-
     }
 }
